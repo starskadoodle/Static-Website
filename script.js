1f0cfbda33094abe0803e6f7f9c1f31f500934b3
lewis cornelius <lewiscornelius0207@gmail.com>
2023-11-14 13:32:21 +0000

    Dark mode changes

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+const DARK_MODE_CLASS = 'dark-mode';
+const DARK_MODE_KEY = 'dark-mode';
+
 function toggleDarkMode() {
-    const isDarkMode = document.body.classList.toggle('dark-mode');
-    document.querySelectorAll('.button').forEach(button => button.classList.toggle('dark-mode', isDarkMode));
-    localStorage.setItem('dark-mode', isDarkMode ? 'true' : 'false');
+    const isDarkMode = document.body.classList.toggle(DARK_MODE_CLASS);
+    document.querySelectorAll('.button').forEach(button => button.classList.toggle(DARK_MODE_CLASS, isDarkMode));
+    localStorage.setItem(DARK_MODE_KEY, String(isDarkMode));
 }
 
 function checkDarkModePreference() {
-    const savedPreference = localStorage.getItem('dark-mode') === 'true';
-    [document.body, ...document.querySelectorAll('.button')].forEach(element => element.classList.toggle('dark-mode', savedPreference));
+    try {
+        const savedPreference = localStorage.getItem(DARK_MODE_KEY) === 'true';
+        [document.body, ...document.querySelectorAll('.button')].forEach(element => element.classList.toggle(DARK_MODE_CLASS, savedPreference));
+    } catch (error) {
+        console.error('Error checking dark mode preference:', error);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', checkDarkModePreference);
 
-const darkModeToggle = document.getElementById('darkModeToggle');
-if (darkModeToggle) darkModeToggle.addEventListener('click', toggleDarkMode);
+const darkModeToggleButton = document.getElementById('darkModeToggle');
+if (darkModeToggleButton) darkModeToggleButton.addEventListener('click', toggleDarkMode);
